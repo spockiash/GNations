6485ba5ede12253dda531d5e21e4ddc1480e7ac8
spockiash <spocky@post.cz>
2022-07-08 17:38:51 +0200

    context menu editor

---
+++ GNations.Dto/MapDisplayBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GNations.Models
+{
+    public class MapDisplayBase
+    {
+        public string? SvgMarkup { get; set; }
+        public string? StyleAttribute { get; set; }
+        public int RelativeTop { get; set; }
+        public int RelativeLeft { get; set; }
+        public int PositionTop { get; set; }
+        public int PositionLeft { get; set; }
+    }
+}
